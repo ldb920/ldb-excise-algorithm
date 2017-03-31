7b0627474df35620f8e6df8916cd0fa4353fe203
dbwind <dbwind@ximalaya.com>
2017-03-31 09:22:53 +0800

    test

---
+++ src/main/java/Test.java
@@ -0,0 +1,15 @@
+/**
+ * Created by ldb920 on 17/3/31.
+ */
+public class Test {
+    public static void test(){
+        int[] intArray = {1,2,3,4,5};
+        for(int i =0; i < intArray.length; i++){
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args){
+        test();
+    }
+}
